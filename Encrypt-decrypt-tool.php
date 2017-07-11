82a182ffbf7e9d226bbd5509b7f1e13ed56b89ed
EresDev <hi@ersedev.com>
2017-07-11 13:32:11 +0500

    implemented shortcode

--- Encrypt-decrypt-tool.php
+++ Encrypt-decrypt-tool.php
@@ -1,11 +1,33 @@
 <?php
-
 /*
 Plugin Name: Encrypt Decrypt Tool
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
+Plugin URI: http://eresdev.com/tools/encrypt-decrypt
+Description: A wordpress plugin to add a tool to encrypt/decrypt
 Version: 1.0
-Author: Ciao
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Author: EresDev
+Author URI: http://eresdev.com
+License: GPL2
 */
+
+function encrypt_decrypt_tool_html(){
+    ?>
+<form name="encrypt_decrypt_tool" action="post">
+    <label>Choose a method:<br><select name="method">
+        <?php
+        $ciphers = openssl_get_cipher_methods();
+        foreach($ciphers as $i=>$cipher) { ?>
+        <option value="<?php echo $i; ?>"><b style="font-weight:bold;">OpenSSL</b> <?php echo $cipher; ?></option>
+            <?php } ?>
+    </select></label><br>
+    <label>Insert your key:<br>
+        <input type="password" name="key"></label>
+    <label>Confirm your key:<br>
+        <input type="password" name="key_confirm"></label>
+    <label>Insert text to Encrypt/Decrypt:<br>
+        <textarea name="main_text"></textarea></label>
+    <input type="submit" name="encrypt" value="Encrypt">
+    <input type="submit" name="decrypt" value="Decrypt">
+</form>
+<?php
+}
+add_shortcode("encrypt_decrypt_tool", "encrypt_decrypt_tool_html");
